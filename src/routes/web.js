1fa141386caa2b6ae59d635a1fc27f56d399dbf4
shinkaym <khoapham1405@gmail.com>
2024-02-25 14:24:46 +0700

    first commit

---
+++ src/routes/web.js
@@ -0,0 +1,12 @@
+import express from "express";
+import { getHomePage } from "../controllers/homeController";
+
+let router = express.Router()
+
+let initWebRoutes = (app) => { 
+  router.get('/', getHomePage)
+
+  return app.use('/', router)
+}
+
+module.exports = initWebRoutes
